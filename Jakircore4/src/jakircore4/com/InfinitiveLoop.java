f4c06232dcf01d51cfca5d29d9bf62fc48a9352e
jakirdc1 <jakirdc1@gmail.com>
2017-10-24 23:10:17 +0600

    Exam practice

---
+++ Jakircore4/src/jakircore4/com/InfinitiveLoop.java
@@ -0,0 +1,8 @@
+package jakircore4.com;
+public class InfinitiveLoop {
+    public static void main(String[] args) {
+        System.out.println("Bangladesh");
+        while(true){
+   }       
+    }
+}
